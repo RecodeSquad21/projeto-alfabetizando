ae4392e3e618a9c2c760f4a860a9bc71fe2817b9
wellyntonsouza <wsods07@gmail.com>
2022-11-13 13:34:40 -0300

    nova versão de tabela para entidade voluntario gerada via ORM/API java

--- Codigos Fonte/Banco de Dados (arquivos SQL)/alfabetizando_voluntario.sql
+++ Codigos Fonte/Banco de Dados (arquivos SQL)/alfabetizando_voluntario.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `alfabetizando` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `alfabetizando`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: alfabetizando
@@ -25,16 +23,18 @@ DROP TABLE IF EXISTS `voluntario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `voluntario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(30) DEFAULT NULL,
-  `sobrenome` varchar(30) DEFAULT NULL,
-  `cidade` varchar(30) DEFAULT NULL,
-  `estado` varchar(30) DEFAULT NULL,
-  `cep` varchar(30) DEFAULT NULL,
-  `email` varchar(30) DEFAULT NULL,
-  `apresentacao` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_voluntario` int NOT NULL AUTO_INCREMENT,
+  `apresentacao` varchar(255) DEFAULT NULL,
+  `cep` int DEFAULT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `sobrenome` varchar(255) DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_voluntario`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `voluntario` (
 
 LOCK TABLES `voluntario` WRITE;
 /*!40000 ALTER TABLE `voluntario` DISABLE KEYS */;
-INSERT INTO `voluntario` VALUES (2,'asad','asd','asd','asd','asd','asd','asd');
+INSERT INTO `voluntario` VALUES (4,'ola!',13084643,'Campinas','joao@gmail.com','São Paulo','Joao','1234','Ricardo','Docência'),(5,'hi!',1308745,'Campinas','bruno@example.com','São Paulo','Bruno','bru123','Silva','Psicologia'),(6,'Bom dia ',13490000,'São Paulo','reafael@example.com','São Paulo','Rafael','rafa123','Oliveira','Psicologia');
 /*!40000 ALTER TABLE `voluntario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-03  9:50:49
+-- Dump completed on 2022-11-13 13:29:50
