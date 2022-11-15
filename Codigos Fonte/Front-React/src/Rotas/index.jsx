2c50f9026c02a98fa4a9d7adebdc49d1fb46c460
wellyntonsouza <wsods07@gmail.com>
2022-11-15 20:22:15 -0300

    adicionando rotas das novas páginas para crud de voluntário

--- Codigos Fonte/Front-React/src/Rotas/index.jsx
+++ Codigos Fonte/Front-React/src/Rotas/index.jsx
@@ -7,17 +7,25 @@ import Home from "../pages/Home";
 import Projetos from "../pages/Projetos";
 import Voluntariado from "../pages/Voluntariado";
 import CadastroAluno from "../pages/CadastroAluno";
+import InfoVolunt from "../pages/InfoVoluntariado";
+import EditVoluntario from "../pages/EditVoluntario";
+import ViewVoluntario from "../pages/ViewVoluntario";
 
-export default function Rotas () {
+
+export default function Rotas() {
     return (
         <BrowserRouter>
-        <Navbar />
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/contato" element={<Contato />} />
                 <Route path="/projetos" element={<Projetos />} />
-                <Route path="/voluntariado" element={<Voluntariado />} />
-                <Route path="/cadastroAluno" element={<CadastroAluno />}/>
+                <Route path="/voluntariado/cadastroVoluntariado" element={<Voluntariado />} />
+                <Route path="/cadastroAluno" element={<CadastroAluno />} />
+                <Route path="/voluntariado" element={<InfoVolunt />} />
+                <Route path="/update/:idVoluntario" element={<EditVoluntario />} />
+                <Route path="/view/:idVoluntario" element={<ViewVoluntario />}/>
+                <Route path="*" element={<h1>Página não encontrada.</h1>} />
             </Routes>
             <Footer />
         </BrowserRouter>
