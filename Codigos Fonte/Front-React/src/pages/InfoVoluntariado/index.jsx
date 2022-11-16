9ba489d16b485e8501427179156a090d05835d05
wellyntonsouza <wsods07@gmail.com>
2022-11-16 16:11:16 -0300

    reorganizando html do cabeçalho da pagina de informação sobre voluntariado

--- Codigos Fonte/Front-React/src/pages/InfoVoluntariado/index.jsx
+++ Codigos Fonte/Front-React/src/pages/InfoVoluntariado/index.jsx
@@ -26,88 +26,94 @@ export default function InfoVolunt() {
   }, [])
 
 
-  const {idVoluntario} = useParams();
+  const { idVoluntario } = useParams();
 
-  const deleteVoluntario = async (idVoluntario) => { 
-    try { 
+  const deleteVoluntario = async (idVoluntario) => {
+    try {
       await axios.delete(`http://localhost:8080/api/alfabetizando/voluntario/${idVoluntario}`)
-      .then((response) => {
-        alert('Voluntário excluido.')
-        listarVoluntarios();
-      })
-    } catch (erro) { 
+        .then((response) => {
+          alert('Voluntário excluido.')
+          listarVoluntarios();
+        })
+    } catch (erro) {
       alert(erro.message)
     }
   }
 
   return (
-    <div className="container">
-      <header id="headInfoVoluntariado">
-        <h1><strong>VOLUNTARIADO</strong></h1>
-        <h3>Três modalidades de voluntariado</h3>
-      </header>
-      <main>
-        <div className="row row-cols-1 row-cols-md-3 g-4" id="cardsVoluntariado">
-          <div className="col">
-            <div className="card h-100">
-              <img src={professora} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">docência</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card h-100">
-              <img src={apoio} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">acolhimento emocional</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card h-100">
-              <img src={mentoria} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">mentorias e apoio pedagógico</h5>
-              </div>
-            </div>
-          </div>
+    <div>
+      <div className="container-flux" id="headInfoVoluntariado">
+        <div className="container bg-transparent">
+          <header>
+            <h1><strong>VOLUNTARIADO</strong></h1>
+            <h3>Três modalidades de voluntariado</h3>
+          </header>
         </div>
-        <h3>Tem interesse em participar? <Link to="cadastroVoluntariado">Cadastre-se aqui!</Link></h3>
-      </main>
-      <div className="py-4">
-        <hr />
-        <h2>Nossos Voluntários</h2>
-        <table className="table table-striped border shadow">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Nome</th>
-              <th scope="col">Sobrenome</th>
-              <th scope="col">Cidade</th>
-              <th scope="col">Estado</th>
-              <th scope="col">Email</th>
-              <th scope="col"> - </th>
-            </tr>
-          </thead>
-          <tbody>
-            {voluntarios.map((voluntario, key) => (
+      </div>
+      <div className="container">
+        <main className="container">
+          <div className="row row-cols-1 row-cols-md-3 g-4" id="cardsVoluntariado">
+            <div className="col">
+              <div className="card h-100">
+                <img src={professora} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">docência</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img src={apoio} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">acolhimento emocional</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img src={mentoria} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">mentorias e apoio pedagógico</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h3>Tem interesse em participar? <Link to="cadastroVoluntariado">Cadastre-se aqui!</Link></h3>
+        </main>
+        <div className="py-4">
+          <hr />
+          <h2>Nossos Voluntários</h2>
+          <table className="table table-striped border shadow">
+            <thead>
               <tr>
-                <th scope="row" key={key}>{key+1}</th>
-                <td>{voluntario.nome}</td>
-                <td>{voluntario.sobrenome}</td>
-                <td>{voluntario.cidade}</td>
-                <td>{voluntario.estado}</td>
-                <td>{voluntario.email}</td>
-                <td>
-                  <Link to={`/view/${voluntario.idVoluntario}`}className="btn btn-out-line-success mx-2">Ver</Link>
-                  <Link to={`/update/${voluntario.idVoluntario}`} className="btn btn-out-line-success mx-2">Editar</Link>
-                  <button className="btn btn-out-line-success mx-2" onClick={() => deleteVoluntario(voluntario.idVoluntario)}>Excluir</button>
-                </td>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Sobrenome</th>
+                <th scope="col">Cidade</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Email</th>
+                <th scope="col"> - </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {voluntarios.map((voluntario, key) => (
+                <tr>
+                  <th scope="row" key={key}>{key + 1}</th>
+                  <td>{voluntario.nome}</td>
+                  <td>{voluntario.sobrenome}</td>
+                  <td>{voluntario.cidade}</td>
+                  <td>{voluntario.estado}</td>
+                  <td>{voluntario.email}</td>
+                  <td>
+                    <Link to={`/view/${voluntario.idVoluntario}`} className="btn btn-out-line-success mx-2">Ver</Link>
+                    <Link to={`/update/${voluntario.idVoluntario}`} className="btn btn-out-line-success mx-2">Editar</Link>
+                    <button className="btn btn-out-line-success mx-2" onClick={() => deleteVoluntario(voluntario.idVoluntario)}>Excluir</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
